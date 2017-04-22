80702cc5c1f1092d424fd94d23b5d4128ad86600
vukse95 <stefan26642@hotmail.com>
2017-04-22 13:08:31 +0200

    Removed unused imports!

--- app/src/main/java/ra210_2014/com/example/student/taskmanager/TaskAdapter.java
+++ app/src/main/java/ra210_2014/com/example/student/taskmanager/TaskAdapter.java
@@ -7,10 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
