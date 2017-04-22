c39521aab981adee8b482e33ca05549fec3cfcdf
vukse95 <stefan26642@hotmail.com>
2017-04-22 15:28:55 +0200

    Added strikethrough on task name!

--- app/src/main/java/ra210_2014/com/example/student/taskmanager/TaskAdapter.java
+++ app/src/main/java/ra210_2014/com/example/student/taskmanager/TaskAdapter.java
@@ -2,6 +2,7 @@ package ra210_2014.com.example.student.taskmanager;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +10,8 @@ import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * Created by Vukse on 13.4.2017..
@@ -21,6 +22,7 @@ public class TaskAdapter extends BaseAdapter {
     //fali holder
     private Context context;
     private ArrayList<TaskModel> tasks;
+    Calendar c = Calendar.getInstance();
 
     public TaskAdapter(Context context) {
         //super(context, R.layout.task_element, item);
@@ -28,8 +30,13 @@ public class TaskAdapter extends BaseAdapter {
         this.tasks = new ArrayList<>();
     }
 
-    public void addTask(TaskModel taskModel){
+    public void addTask(TaskModel taskModel) {
         tasks.add(taskModel);
+        notifyDataSetChanged();
+    }
+
+    public void removeTask(TaskModel taskModel){
+        tasks.remove(taskModel);
         notifyDataSetChanged();
     }
 
@@ -42,9 +49,9 @@ public class TaskAdapter extends BaseAdapter {
     public Object getItem(int position) {
         Object rv = null;
 
-        try{
+        try {
             rv = tasks.get(position);
-        }catch (IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
             e.printStackTrace();
         }
 
@@ -58,9 +65,14 @@ public class TaskAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-       View view = convertView;
+        View view = convertView;
 
-        if(view == null){
+        //ne dozvoljava direktnu komparaciju??
+        int CurrentDay = c.get(Calendar.DAY_OF_MONTH);
+        int CurrentMont = c.get(Calendar.MONTH);
+        int CurrentYear = c.get(Calendar.YEAR);
+
+        if (view == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(
                     Context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.task_element, null);
@@ -68,20 +80,43 @@ public class TaskAdapter extends BaseAdapter {
             holder.nazivZadatka = (TextView) view.findViewById(R.id.title);
             holder.datum = (TextView) view.findViewById(R.id.date);
             holder.podsetnik = (CheckBox) view.findViewById(R.id.reminder);
-            holder.prioritet =  view.findViewById(R.id.priority);
+            holder.prioritet = view.findViewById(R.id.priority);
             view.setTag(holder);
         }
 
         //ovde budzim za boju i ostalo
 
         TaskModel tModel = (TaskModel) getItem(position);
-        ViewHolder holder = (ViewHolder) view.getTag();
+        final ViewHolder holder = (ViewHolder) view.getTag();
+
+
         holder.nazivZadatka.setText(tModel.nameOfAssignment);
-        //promeniti da postoji juce, danas, sutra, itd...
-        holder.datum.setText(tModel.day + "." + tModel.month + "." + tModel.year);
-        holder.podsetnik.setChecked(tModel.reminder);
-        //promeniti da na osnovu priorityFlag menja jednu od 3 boja
-        switch(tModel.priorityFlag){
+
+        if (tModel.year == CurrentYear && tModel.month == CurrentMont) {
+            if (CurrentDay - tModel.day > 7) {
+                //TODO: Napisi datum
+            } else {
+                if (tModel.day == CurrentDay) {
+                    holder.datum.setText("Danas");
+                } else if (CurrentDay + 1 == tModel.day) {
+                    holder.datum.setText("Sutra");
+                }
+            }
+        }
+
+        holder.podsetnik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.podsetnik.isChecked()) {
+                    holder.nazivZadatka.setPaintFlags(holder.nazivZadatka.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                } else {
+                    holder.nazivZadatka.setPaintFlags(holder.nazivZadatka.getPaintFlags() & ~Paint.STRIKE_THRU_TEXT_FLAG);
+                }
+            }
+        });
+
+
+        switch (tModel.priorityFlag) {
             case 1:
                 holder.prioritet.setBackgroundColor(Color.RED);
                 break;
@@ -101,7 +136,7 @@ public class TaskAdapter extends BaseAdapter {
         public TextView nazivZadatka = null;
         public TextView datum = null;
         public CheckBox podsetnik = null;
-        public View  prioritet = null;
+        public View prioritet = null;
     }
 
 
