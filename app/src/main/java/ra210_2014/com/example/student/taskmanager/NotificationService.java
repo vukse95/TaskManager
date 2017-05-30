15292e0801c1923c5e179dcac892769d42a0d878
vukse95 <stefan.vukanovic@hotmail.com>
2017-05-30 09:09:41 +0200

    Almost done, have errors ! :D

---
+++ app/src/main/java/ra210_2014/com/example/student/taskmanager/NotificationService.java
@@ -0,0 +1,78 @@
+package ra210_2014.com.example.student.taskmanager;
+
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.logging.LogRecord;
+
+/**
+ * Created by stefa on 5/29/2017.
+ */
+
+public class NotificationService extends Service {
+    private TaskModel[] tasks = null;
+    private final IBinder myBinder = new MyLocalBinder();
+    NotificationManager mng;
+    NotificationCompat.Builder builder = null;
+    Calendar calender = Calendar.getInstance();
+    int Day = calender.get(Calendar.DAY_OF_MONTH);
+    int Month = calender.get(Calendar.MONTH);
+    int Year = calender.get(Calendar.YEAR);
+    int Hour = calender.get(Calendar.HOUR);
+    int Min = calender.get(Calendar.MINUTE);
+
+    public NotificationService() {
+        final Handler handler = new Handler();
+        Runnable thread = new Runnable() {
+            @Override
+            public void run() {
+                if (tasks != null) {
+                    for (TaskModel i : tasks) {
+                        if (i.isReminder()) {
+                            if (i.getDay() == Day && i.getMonth() == Month && i.getYear() == Year
+                                    && i.getHour() == Hour){
+                                    if (i.getMinute() - Min <= 15 && i.getMinute() - Min >= 0){
+                                        mng = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                                        builder = new NotificationCompat.Builder(NotificationService.this)
+                                                .setSmallIcon(R.mipmap.ic_launcher)
+                                                .setContentText("15 min to isteka taska!!")
+                                                .setContentTitle("TaskManager");
+                                    }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        thread.run();
+
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return myBinder;
+    }
+
+
+    public class MyLocalBinder extends Binder {
+        NotificationService getService() {
+            return NotificationService.this;
+        }
+
+        public void setTasks(TaskModel[] lista) {
+            tasks = lista;
+        }
+    }
+}
+
